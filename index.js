16350207972b499f260a1cf829f8bc3621ccf36e
Jefferon AFANOU <jefferson.afanou@gmail.com>
2020-09-16 10:01:39 +0200

    Avancement TP

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('** Administration Hotel **');
+var presentation = require('./presentation.js');
+
+console.log("** Application Hotel **");
+presentation.start();
---
+++ presentation.js
@@ -0,0 +1,7 @@
+function start (){
+    console.log("Menu")
+    console.log("1. Lister les clients");
+    console.log("99. Sortir");
+}
+
+exports.start = start;
